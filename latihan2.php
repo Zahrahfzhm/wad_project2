a4f657a056e925d2270e27d87394afa93c175cf1
Zahrahfzhm <116279310+Zahrahfzhm@users.noreply.github.com>
2023-10-20 18:55:04 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name=viewport" contenct="width=device-width, initial-scale=1">
+        <title>Latihan 2 PHP</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1> Tugas Latihan 2</h1>
+        <div class="input-group mb-3">
+            <span class="input-group-text">$</span>
+            <span class="input-group-text">0.00</span>
+            <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+        </div>
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupSelect01">Umur : </label>
+            <select class="form-select" id="inputGroupSelect01">
+                <option selected>Choose...</option>
+                <option value="1">12-18 Tahun</option>
+                <option value="2">19-25 Tahun</option>
+                <option value="3">26-30 Tahun</option>
+            </select>
+        </div>
+    </body>
+</html>
